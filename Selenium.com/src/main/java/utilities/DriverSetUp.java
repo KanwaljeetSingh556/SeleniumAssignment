aaad65430e5de9de955661b205db280f20ef59d4
KanwaljeetSingh556 <92438895+KanwaljeetSingh556@users.noreply.github.com>
2022-05-30 20:14:53 +0530

    Add files via upload

---
+++ Selenium.com/src/main/java/utilities/DriverSetUp.java
@@ -0,0 +1,45 @@
+package utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class DriverSetUp 
+{
+	ReadPropertiesFile read = new ReadPropertiesFile("Properties/assignment.properties");
+	WebDriver  driver;
+	
+	
+	public WebDriver GetDriver()
+	{
+	  if(read.ReadData("Browser").equalsIgnoreCase("Chrome")) {
+		   driver = WebDriverManager.chromedriver().create();
+		
+	  }
+	  
+	  else if(read.ReadData("Browser").equalsIgnoreCase("edge")) {
+		   driver = WebDriverManager.edgedriver().create();
+	  }
+	  
+	  else {
+		    driver =  WebDriverManager.chromedriver().create(); 
+	  }
+	return driver;
+	
+	}
+	
+	public void GetURL() 
+	{
+		 driver.get(read.ReadData("URL"));
+		 driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("hello google");
+		
+	}
+	
+
+	
+
+}
---
+++ Selenium.com/src/test/java/base/BaseClass.java
@@ -0,0 +1,15 @@
+package base;
+
+import utilities.DriverSetUp;
+
+public class BaseClass {
+
+	public static void main(String[] args)
+	{
+		DriverSetUp set = new DriverSetUp();
+		set.GetDriver();
+		set.GetURL();
+	
+	}
+
+}
